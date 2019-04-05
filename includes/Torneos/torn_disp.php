28506b0998b4b4820f385ac2d15c1ecf1b23e592
Daniel Grado <dangrad08@gmail.com>
2019-04-05 22:50:29 +0200

    Update torn_disp.php

--- includes/Torneos/torn_disp.php
+++ includes/Torneos/torn_disp.php
@@ -1,10 +1,31 @@
+<?php
+require_once __DIR__ .'/../comun/config.php';
+require_once __DIR__ .'/../productos/Producto.php';
+?>
 <div id="torneos_jug">
 	<h2>Torneos disponibles</h2>
-	<tabla class="torneo_table">
+	<table class="torneo_table">
 		<tr>
-		    <td>Juego</td>
-		    <td>Fecha</td>
+		    <th>Juego</th>
+		    <th>Fecha</th>
 	 	</tr>
-	 	<!--Aquí hacer un bucle for con todos los juegos disponibles-->
-	</tabla>
+		<?php
+		$app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+		$query = sprintf("SELECT * FROM torneo_jugando t WHERE t.ronda = '%s'", $conn->real_escape_string("clasificacion"));
+        $rs = $conn->query($query);
+		
+		
+		$data = $rs->fetch_assoc();
+		foreach($data as $cursor){
+			$idprod = @$cursor["idJuego"];
+			$fecha = @$cursor["dia_jugado"];
+			$producto = Product::buscaProduco($idprod);
+			echo "<tr>";
+			echo "<td>" . $producto->nombreProd() . "</td>";
+			echo "<td>" . $fecha . "</td>";
+			echo "</tr>";
+	}	
+		?>		
+	</table>
 </div>
