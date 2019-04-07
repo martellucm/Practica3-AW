ccae734dbd7acf67405ef8e7684a730a63c327d9
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-04-07 20:32:43 +0200

    Update qualifyingTable.php

--- includes/Torneos/qualifyingTable.php
+++ includes/Torneos/qualifyingTable.php
@@ -8,18 +8,18 @@
 	$fecha = $_GET['filtroF'];
 	$juego = $_GET['filtroJ'];
 	if($fecha != "no" && $juego != "no"){
-		$query = mysql_query("SELECT * FROM torneo_jugando t WHERE t.dia_jugado = $fecha, t.idJuego = $juego AND t.ronda = clasificacion") or die(mysql_error());
+		$query = mysqli_query("SELECT * FROM torneo_jugando t WHERE t.dia_jugado = $fecha, t.idJuego = $juego AND t.ronda = clasificacion") or die(mysqli_error());
 	}
 	else if($fecha != "no"){
-		$query = mysql_query("SELECT * FROM torneo_jugando t WHERE t.dia_jugado = $fecha AND t.ronda = clasificacion ORDER BY t.idJuego ASC") or die(mysql_error());
+		$query = mysqli_query("SELECT * FROM torneo_jugando t WHERE t.dia_jugado = $fecha AND t.ronda = clasificacion ORDER BY t.idJuego ASC") or die(mysqli_error());
 	}
 	else if($juego != "no"){
-		$query = mysql_query("SELECT * FROM torneo_jugando t WHERE t.idJuego = $juego AND t.ronda = clasificacion ORDER BY t.dia_jugado ASC") or die(mysql_error());
+		$query = mysqli_query("SELECT * FROM torneo_jugando t WHERE t.idJuego = $juego AND t.ronda = clasificacion ORDER BY t.dia_jugado ASC") or die(mysqli_error());
 	}
 	else{
-		$query = mysql_query("SELECT * FROM torneo_jugando AND t.ronda = clasificacion ORDER BY t.dia_jugado ASC") or die(mysql_error());
+		$query = mysqli_query("SELECT * FROM torneo_jugando AND t.ronda = clasificacion ORDER BY t.dia_jugado ASC") or die(mysqli_error());
 	}
-    $data = mysql_fetch_array($query); 
+    $data = mysqli_fetch_array($query); 
 	
 	foreach($data as $cursor){
 		$producto = Producto::buscaProductoID($cursor['idJuego']);
