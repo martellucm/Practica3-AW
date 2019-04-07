fbe7c658ac5df58c6872911150cb8fde941cdfd4
Daniel Grado <dangrad08@gmail.com>
2019-04-07 20:10:36 +0200

    Update torn_disp.php

--- includes/Torneos/torn_disp.php
+++ includes/Torneos/torn_disp.php
@@ -1,12 +1,14 @@
 <?php
 require_once __DIR__ .'/../comun/config.php';
 require_once __DIR__ .'/../productos/Producto.php';
+require_once __DIR__ .'/../usuarios/Usuario.php';
 ?>
 <div id="torneos_jug">
 	<h2>Torneos disponibles</h2>
 	<table class="torneo_table">
 		<tr>
 		    <th>Juego</th>
+		    <th>Usuario</th>
 		    <th>Fecha</th>
 	 	</tr>
 		<?php
@@ -20,10 +22,13 @@ require_once __DIR__ .'/../productos/Producto.php';
 		}
 		for($i = 0; $i < count($rows); $i++){
 			$idprod = @$rows['i']["idJuego"];
+			$idUsu = @rows['i']["id_jugad_jugan"];
 			$fecha = @$rows['i']["dia_jugado"];
 			$producto = Product::buscaProduco($idprod);
+			$usuario = Usuario::buscaUsuario($idUsu);
 			echo "<tr>";
 			echo "<td>" . $producto->nombreProd() . "</td>";
+			echo "<td>" . $usuario->nombre() . "</td>";
 			echo "<td>" . $fecha . "</td>";
 			echo "</tr>";
 		}	
